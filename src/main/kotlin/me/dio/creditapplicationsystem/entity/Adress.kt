a408d2984329f528fe2abc1db3e4d10aef800d07
renanmello <mellorenan19@gmail.com>
2023-05-26 12:09:15 -0300

    create entitys

--- src/main/kotlin/me/dio/creditapplicationsystem/entity/Adress.kt
+++ src/main/kotlin/me/dio/creditapplicationsystem/entity/Adress.kt
@@ -1,8 +1,12 @@
 package me.dio.creditapplicationsystem.entity
 
+import jakarta.persistence.Column
+import jakarta.persistence.Embeddable
+
+@Embeddable
 data class Address (
-    var zipCode: String = "",
-    var street: String = ""
+    @Column(nullable = false) var zipCode: String = "",
+    @Column(nullable = false) var street: String = ""
         )
 
 
--- src/main/kotlin/me/dio/creditapplicationsystem/entity/Customer.kt
+++ src/main/kotlin/me/dio/creditapplicationsystem/entity/Customer.kt
@@ -1,16 +1,21 @@
 package me.dio.creditapplicationsystem.entity
 
+import jakarta.persistence.*
 
 
+@Entity
+@Table(name = "customer")
 data class Customer(
-    var firstName: String = "",
-    var lastName: String = "",
-    var cpf: String,
-    var email: String = "",
-    var password: String = "",
-    var address: Address = Address(),
-    var credits: List<Credit> = mutableListOf(),
-    var id: Long? = null
+    @Column(nullable = false) var firstName: String = "",
+    @Column(nullable = false) var lastName: String = "",
+    @Column(nullable = false , unique = true) var cpf: String,
+    @Column(nullable = false,unique = true) var email: String = "",
+    @Column(nullable = false) var password: String = "",
+    @Column(nullable = false) @Embedded var address: Address = Address(),
+    @Column(nullable = false) @OneToMany(fetch = FetchType.LAZY ,
+        cascade = arrayOf(CascadeType.REMOVE , CascadeType.PERSIST), mappedBy = "customer"
+    ) var credits: List<Credit> = mutableListOf(),
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) var id: Long? = null
 
 
-    )
+)
